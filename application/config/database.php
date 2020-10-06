ca8313da97a50d1991ca22775b86422341164a20
m4s3s7 <ms.vasilis1@gmail.com>
2020-10-07 00:08:15 +0300

    database update ssl server

--- application/config/database.php
+++ application/config/database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "mysql";
+$username = "c0arabalars";
+$password = "Y7ckrtd0vfd..";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
