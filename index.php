b53b6061695b886ba970846fd2d3d7ec6f78ecbc
m4s3s7 <ms.vasilis1@gmail.com>
2020-10-08 00:23:52 +0300

    update Final route stock

--- index.php
+++ index.php
@@ -5,23 +5,28 @@ header("Access-Controll-Allow-Origin: *");
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+$uri    = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
-switch ($method){
+$exp = explode('/', $uri);
+$param = $exp[2];
+if($param == 'stock'){
+  switch ($method){
+    case 'GET':
+    include_once 'application/controllers/GetController.php';
+    $get = new GetController();
+    $get->get();
+    break;
+    case 'POST':
+    include_once 'application/controllers/PostController.php';
+    $post = new PostController();
+    $post->post();
 
-  case 'GET':
-  include_once 'application/controllers/GetController.php';
-  $get = new GetController();
-  $get->get();
-  break;
-  case 'POST':
-  include_once 'application/controllers/PostController.php';
-  $post = new PostController();
-  $post->post();
-
-  break;
-  default:
-  echo "Unknown request method.";
-  break;
+    break;
+    default:
+    echo "Unknown request method.";
+    break;
+  }
+}else {
   
 }
